f088994bd1252843f6e4a1a59ca2bcee1b207b40
Henrik Finsberg <henriknf@simula.no>
2020-06-25 14:25:17 +0200

    Add calulator and tests

--- tests/test_calculator4you.py
+++ tests/test_calculator4you.py
@@ -1,24 +1,31 @@
-#!/usr/bin/env python
+import random
+import pytest  # pip install pytest
 
-"""Tests for `calculator4you` package."""
+from calculator4you import calculator4you as calculator
 
-import pytest
-
-
-from calculator4you import calculator4you
+tol = 1e-14
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def test_add():
+    assert abs(calculator.add(1, 1) - 2) < tol
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_subtract():
+    assert abs(calculator.subtract(2, 1) - 1) < tol
+
+
+def test_divide():
+    assert abs(calculator.divide(1, 2) - 0.5) < tol
+
+
+def test_divide_raises_ZeroDivisionError():
+
+    with pytest.raises(ZeroDivisionError):
+        calculator.divide(1, 0)
+
+
+@pytest.mark.parametrize("execution_number", range(3))
+def test_add_random_pytest(execution_number):
+    x = random.uniform(0, 100)
+    y = random.uniform(0, 100)
+    assert abs(calculator.add(x, y) - (x + y)) < tol
